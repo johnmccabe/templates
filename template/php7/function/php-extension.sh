5768b0bcf472710258864fe4f49a874ca4d736b0
John McCabe <john@johnmccabe.net>
2018-09-27 11:46:10 +0100

    Add some usage docs to new output
    and also to the php-extension.sh file.
    
    Signed-off-by: John McCabe <john@johnmccabe.net>

--- template/php7/function/php-extension.sh
+++ template/php7/function/php-extension.sh
@@ -2,4 +2,8 @@
 
 echo "Installing PHP extensions"
 # Add your extensions in here
+# See the PHP base image README.md for instructions:
+# - https://github.com/docker-library/docs/blob/master/php/README.md#how-to-install-more-php-extensions
+# As this is an Alpine based image you can also manage packages with apk.
+
 #docker-php-ext-install mysqli
